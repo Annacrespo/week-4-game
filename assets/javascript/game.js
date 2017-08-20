50dcc98480a63931f5a4215f7569b5f62549b23d
annacrespo12 <annacrespo12@gmail.com>
2017-08-19 23:17:46 -0700

    rupes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,9 +6,7 @@
 // $(".crystal-image").on("click", function() {
 
 //music autoloop on document load
-$(document).ready(function() {
-    $("#02-kokiri").get(0).play();
-});
+
 var wins = 0;
 var losses = 0;
 var guesses = 9;
@@ -22,3 +20,23 @@ $('#button').on('click', function generate() {
   $('#number').text(Math.floor(Math.random() * 10) + 1);
   console.log()
 });
+
+// $(document).ready(function() {
+
+//       // When random-button is clicked...
+//       $("#rupee1").on("click", function() {
+
+//         // Create a string which will hold the lottery number
+//         var lottoNumber = "";
+
+//         // Then initiate a loop to generate 9 separate numbers
+//         for (var i = 0; i < 9; i++) {
+
+//           // For each iteration, generate a new random number between 1 and 9.
+//           var random = Math.floor(Math.random() * 9) + 1;
+
+//           // Take this number and then add it to the rest of the string.
+//           // In essence, we are iteratively building a string of numbers. (e.g. First: 1, Second: 13, Third: 135, etc.)
+//           lottoNumber = random + lottoNumber;
+
+//         }

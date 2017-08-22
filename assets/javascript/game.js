1176a885f1c34acad93e170acaf112dffb58045e
annacrespo12 <annacrespo12@gmail.com>
2017-08-21 22:35:09 -0700

    final push

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,42 +1,87 @@
-// $("randomNumber").text(targetNumber);
-
-// var counter = 0;
-// var increment = numberOptions[Math.round(Math.random())];
-
-// $(".crystal-image").on("click", function() {
-
-//music autoloop on document load
-
+var playerNum = [];
+var intArr = [];
+var arrSum = 0;
 var wins = 0;
 var losses = 0;
-var guesses = 9;
-var guessed = [];
+var number = 0;
 
-function reset() {
+$('#arrSum').text(arrSum);
+$('#number').text(number);
 
-}	
-/*random number generator*/
+//Generate a random number for the player to guess
 $('#button').on('click', function generate() {
-  $('#number').text(Math.floor(Math.random() * 10) + 1);
-  console.log()
+  $('#number').text(Math.floor(Math.random() * 100) + 1);
+  let number = document.getElementById('number').innerText;
+  console.log(number)
+});
+let reset = function() {
+  number = 0;
+  arrSum = 0;
+}
+
+// When each rupee is clicked it will generate a random number between certain numer space(ex: 1-10)
+// It also executes a function that then gets the number and saves it in a variable to be used when adding up the player score
+
+//Rupee one
+$('#rupee1').on('click', function generate() {
+  $('#rupee1random').text(Math.floor(Math.random()*(10-1+1)+1));
+});
+$('#rupee1').on('click', function add() {
+  let rup1 = document.getElementById('rupee1random').innerText;
+  playerNum.push(rup1);
+  allCheck();
 });
 
-// $(document).ready(function() {
+// Rupee two
+$('#rupee2').on('click', function generate() {
+  $('#rupee2random').text(Math.floor(Math.random()*(20-11+1)+11));
+});
+$('#rupee2').on('click', function add() {
+  let rup2 = document.getElementById('rupee2random').innerText;
+  playerNum.push(rup2);
+  allCheck();
+});
 
-//       // When random-button is clicked...
-//       $("#rupee1").on("click", function() {
+// Rupee three
+$('#rupee3').on('click', function generate() {
+  $('#rupee3random').text(Math.floor(Math.random()*(30-21+1)+21));
+});
+$('#rupee3').on('click', function add() {
+  let rup3 = document.getElementById('rupee3random').innerText;
+  playerNum.push(rup3);
+  allCheck();
+});
 
-//         // Create a string which will hold the lottery number
-//         var lottoNumber = "";
+//Rupee four
+$('#rupee4').on('click', function generate() {
+  $('#rupee4random').text(Math.floor(Math.random()*(40-31+1)+31));
+});
+$('#rupee4').on('click', function add() {
+  let rup4 = document.getElementById('rupee4random').innerText;
+  playerNum.push(rup4);
+  allCheck();
+});
 
-//         // Then initiate a loop to generate 9 separate numbers
-//         for (var i = 0; i < 9; i++) {
+// the all check function converts all strings to ints, adds the player score up, pushes it to the html and checks if 
+//the player has gone over or won the game
+let allCheck = function() {
+    for(var i = 0; i < playerNum.length; i++){
+      intArr.push(parseInt(playerNum[i]))};
+    var arrSum = intArr.reduce((a, b) => a + b, 0);
+    var arrStringSum = arrSum.toString();
+    $('#arrSum').text(arrSum);
+    let number = document.getElementById('number').innerText;
+    if (arrSum === number) {
+    wins++;
+    $("#wins").append('<img src="assets/images/green-rupee-win.png" alt="green-rupee" style="width:50px;">');
+    alert("You win!");
+  } else if (arrSum > number) {
+    losses++;
+    $('#lives img:last').remove();
+    alert("You lose!");
+  }
+  if(losses < 1){
+    location.reload();
+  }
+}
 
-//           // For each iteration, generate a new random number between 1 and 9.
-//           var random = Math.floor(Math.random() * 9) + 1;
-
-//           // Take this number and then add it to the rest of the string.
-//           // In essence, we are iteratively building a string of numbers. (e.g. First: 1, Second: 13, Third: 135, etc.)
-//           lottoNumber = random + lottoNumber;
-
-//         }
